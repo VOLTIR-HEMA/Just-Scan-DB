8b5c8a45067a292d0678482fe3cef210f7b5470d
VOLTIR-HEMA <96622772+VOLTIR-HEMA@users.noreply.github.com>
2026-07-06 17:03:26 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,45 @@
+// هذا الملف ضروري لعمل الإشعارات في الخلفية
+// قم بإنشاء ملف جديد بهذا الاسم وضعه في المجلد الرئيسي لمشروعك
+
+// استيراد مكتبات Firebase اللازمة
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js");
+
+// ! هام جداً: قم بلصق كود الإعدادات الذي نسخته من موقع Firebase هنا
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4hRnaIq3Dn4o8cqgTVk3vg_XXmwyqOlA",
+  authDomain: "my-assistant-7c330.firebaseapp.com",
+  databaseURL: "https://my-assistant-7c330-default-rtdb.firebaseio.com",
+  projectId: "my-assistant-7c330",
+  storageBucket: "my-assistant-7c330.firebasestorage.app",
+  messagingSenderId: "509561059857",
+  appId: "1:509561059857:web:edeb6a4a2526f15bb00fb0",
+  measurementId: "G-ZPM602TWDY"
+};
+
+// تهيئة تطبيق فايربيز
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// معالجة الإشعارات الواردة عندما يكون التطبيق في الخلفية
+messaging.onBackgroundMessage((payload) => {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+
+  // Firebase Cloud Functions ترسل البيانات في حقل data وليس notification
+  // عندما يكون التطبيق في الخلفية
+  const notificationTitle = payload.data.title;
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: "https://i.imgur.com/rgy8E8S.png", // أيقونة الصيدلية
+    // إضافة صوت وتفاعل
+    sound: "https://assets.mixkit.co/sfx/preview/mixkit-correct-answer-tone-2870.mp3",
+    vibrate: [200, 100, 200], // اهتزاز
+    actions: [
+      { action: 'open_app', title: '🚀 فتح التطبيق' }
+    ]
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
